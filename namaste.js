8926b65ab80b3b1097a29e71caf97601e960c02d
Sanskar Chirania <chiraniasanskar@gmail.com>
2024-04-04 10:38:48 +0530

    First Commit

---
+++ namaste.js
@@ -0,0 +1,7 @@
+
+const heading = React.createElement("h1", {} , "Hallo");
+const heading2 = React.createElement("h1", {} , "Hallo");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render([heading, heading2]);
