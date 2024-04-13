d911753fd56cf593a3fcfe1545f9e2ea122260aa
Sanskar Chirania <sanskar125112@gmail.com>
2024-04-13 12:21:56 +0530

    Assignment-3: Learned functional component, elemnet, and rendered it

--- namaste.js
+++ namaste.js
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import companyLogo from './images/nike_logo.png'
 
-const heading = React.createElement("h1", {id: "title"} , "Hallo");
-const heading2 = React.createElement("h1", {id: "title"} , "Hallo");
+const heading = <h1 key="123">Hello</h1>;
+
+const Logo = () => (    
+    <div>
+        <img src={companyLogo} alt=""></img>
+        <div></div>
+        <img src="" alt=""></img>
+    </div>
+)
+
+const Heading = () => (    
+    <div>
+        <Logo />
+        {heading}
+        <h3 key="125">Hello</h3>
+    </div>
+)
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render([heading, heading2]);
+root.render(<Heading />);
